dd4653fa25e3ed9002961f583f7da05e0555c3a1
Bamidele Olaoluwa <bamideleolaoluwa95@gmail.com>
2025-07-17 12:57:58 +0100

    new

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,6 +1,13 @@
 export const Data = [
     {id: 1, name: 'john'},
-    {id: 1, name: 'peter'},
-    {id: 1, name: 'suzan'},
-    {id: 1, name: 'anna'},
+    {id: 2, name: 'peter'},
+    {id: 3, name: 'suzan'},
+    {id: 4, name: 'anna'},
+]
+
+export const people = [
+    {id: 1, name: 'bob', nickName: 'stud Muffin'},
+    {id: 2, name: 'peter', nickName: 'stud Muffin'},
+    {id: 3, name: 'bob', nickName: 'stud Muffin'},
+    {id: 4, name: 'bob', nickName: 'stud Muffin'},
 ]
--- src/ErrorExample.jsx
+++ src/ErrorExample.jsx
@@ -2,27 +2,38 @@ import React, {useState} from "react";
 import { Data } from "./Data";
 
 const ErrorExample = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [people, setPeople] = useState(Data)
 
-    const handleClick = () => {
-      setCount(count + 1)
+    // const handleClick = () => {
+    //   setCount(count + 1)
 
-    }
+    // }
+
+    const removeItem = () => {}
+    const clearAllItems = () => {}
   return (
     <div>
-        <h4>You clicked {count} times</h4>
+    
+        {/* <h4>You clicked {count} times</h4>
         <button type='button' onClick={handleClick} className="btn"> click me</button>
+     */}
 
-        {people.map((person)=>{
-          const {id, name} = person
-          console.log(person)
-          return <div key={id}>
-            <h4>{name}</h4>
-          </div>
-        })}
+    
       
-    </div>
+        {people.map((person)=>{
+          const {id, name} = person;
+          console.log(person)
+          return( <div key={id}>
+            <h4>{name}</h4>
+            <button type="button">remove</button>
+          </div>)
+        })}
+        <button type="button" style={{marginTop: '2rem'}} className="btn">Clear Items</button>
+      
+      </div>
+      
+    
   )
 }
 
